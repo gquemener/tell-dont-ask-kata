fddda7ad1f2dd9f2fa8367117a542e97096675cf
Gildas Quéméner <gildas.quemener-ext@nickel.eu>
2026-01-30 12:58:40 +0100

    readonly methods

--- src/domain/OrderItem.ts
+++ src/domain/OrderItem.ts
@@ -1,10 +1,10 @@
 import Product from './Product';
 
 class OrderItem {
-  private tax: number
-  private taxedAmount: number
+  private readonly tax: number
+  private readonly taxedAmount: number
 
-  constructor(private product: Product, private quantity: number) {
+  constructor(private readonly product: Product, private readonly quantity: number) {
     this.taxedAmount = Math.round(product.getUnitaryTaxedAmount() * quantity * 100) / 100;
     this.tax = product.getUnitaryTax() * quantity;
   }
@@ -13,34 +13,17 @@ class OrderItem {
     return this.product;
   }
 
-  public setProduct(product: Product): void {
-    this.product = product;
-  }
-
   public getQuantity(): number {
     return this.quantity;
-  }
-
-  public setQuantity(quantity: number): void {
-    this.quantity = quantity;
   }
 
   public getVATIncludedAmount(): number {
     return this.taxedAmount;
   }
 
-  public setTaxedAmount(taxedAmount: number): void {
-    this.taxedAmount = taxedAmount;
-  }
-
   public getTax(): number {
     return this.tax;
-  }
-
-  public setTax(tax: number): void {
-    this.tax = tax;
   }
 }
 
 export default OrderItem;
-
